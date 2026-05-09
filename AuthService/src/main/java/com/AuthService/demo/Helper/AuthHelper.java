4ab1cc2be221ac4a9051bc04b68a372ea51d431a
Saif El-Din Mohamed <167351817+Saif13m05@users.noreply.github.com>
2026-05-10 02:29:33 +0300

    Add files via upload

---
+++ AuthService/src/main/java/com/AuthService/demo/Helper/AuthHelper.java
@@ -0,0 +1,44 @@
+package com.AuthService.demo.Helper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthHelper {
+
+    private static final String SECRET = "my-secret-key";
+
+    public void checkInternal(HttpServletRequest request) {
+        String secret = request.getHeader("X-Internal-Secret");
+        if (!SECRET.equals(secret)) {
+            throw new RuntimeException("Forbidden: direct access denied");
+        }
+    }
+
+    public void checkAdmin(HttpServletRequest request) {
+        String role = request.getHeader("X-User-Role");
+        if (!"ADMIN".equalsIgnoreCase(role)) {
+            throw new RuntimeException("Forbidden: Admin only");
+        }
+    }
+
+    public void checkOwnerOrAdmin(HttpServletRequest request, String email) {
+        String role = request.getHeader("X-User-Role");
+        String currentUser = request.getHeader("X-User-Email");
+
+        if (!email.equals(currentUser) && !"ADMIN".equalsIgnoreCase(role)) {
+            throw new RuntimeException("Forbidden: Not allowed");
+        }
+    }
+    
+    public void checkOwnerByIdOrAdmin(HttpServletRequest request, String id) {
+        String role = request.getHeader("X-User-Role");
+        String currentUser = request.getHeader("X-User-Id");
+
+        if (!id.equals(currentUser) && !"ADMIN".equalsIgnoreCase(role)) {
+            throw new RuntimeException("Forbidden: Not allowed");
+        }
+    }
+    
+    
+}
